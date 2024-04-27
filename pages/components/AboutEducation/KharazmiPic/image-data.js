58e67a47b021e1748aad709984fe1eb71fefe50b
Mohammad H. Mazinani <mhmazinani1994@gmail.com>
2024-04-27 16:16:40 +0330

    fix: Localize photos

--- pages/components/AboutEducation/KharazmiPic/image-data.js
+++ pages/components/AboutEducation/KharazmiPic/image-data.js
@@ -1,5 +1,5 @@
 export const images = [
-    "https://lh3.googleusercontent.com/p/AF1QipOZveDqN7YaOLbV-HGlptb9tAey5IDWSpJEI8Vn=s680-w680-h510",
-    "https://lh3.googleusercontent.com/p/AF1QipN1M6-E0g0gsfc9v4b4HzaezcDrhctfkUPODjxG=s680-w680-h510",
-    "https://lh3.googleusercontent.com/p/AF1QipMOmymnkXqHJcUN4c4Hc9oVHLEjEb0fmGIdJXUq=s680-w680-h510"
+    "/images/kharazmi1.png",
+    "/images/kharazmi2.png",
+    "/images/kharazmi3.png",
   ];
